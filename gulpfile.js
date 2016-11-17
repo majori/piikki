8a1d6201572b300a484e233e9d070cb821d78a90
Antti Kivimäki <majoriz11@gmail.com>
2016-11-17 18:26:40 +0200

    Make build process

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp  = require('gulp');
+const ts    = require('gulp-typescript');
+
+const SOURCE_DIR = './src';
+const BUILD_DIR = './dist'
+
+const CONFIG = {
+    typescript: {
+        target: 'ES6'
+    }
+}
+
+gulp.task('build-typescript', () => gulp
+    .src(`${SOURCE_DIR}/**/*.ts`)
+    .pipe(ts(CONFIG.typescript))
+    .pipe(gulp.dest(BUILD_DIR))
+);
+
+gulp.task('watch', () => {
+    gulp.watch(`${SOURCE_DIR}/**/*.ts`, ['build-typescript'])
+})
+
+gulp.task('build', ['build-typescript']);
+gulp.task('default', ['build', 'watch']);
