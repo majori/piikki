cda856e72451797323e6ce7e5007af73b7b87c31
Antti Kivimäki <majoriz11@gmail.com>
2016-11-17 20:43:49 +0200

    Add Readme and use body-parser

---
+++ README.md
@@ -0,0 +1,6 @@
+## Get started
+
+1. Clone this repository and run `npm install`
+2. Install gulp globally `npm install gulp -g`
+3. Build `gulp build`
+4. Start server `node dist/index.js`
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 import routes from './routes';
 
 const app = express();
 
 const ENV = process.env.NODE_ENV || 'development';
+const HOSTNAME = process.env.PIIKKI_HOSTNAME || 'localhost';
 const PORT = process.env.PIIKKI_PORT || 3000;
 
 if (ENV === 'development') {
     // Use dev middlewares
 }
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // Config routes
 routes(app);
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
+app.listen(PORT, HOSTNAME, () => {
+    console.log(`Server listening on http://${HOSTNAME}:${PORT}`);
 });
