ba19c41de3da74bf26f76a435bc412f43b5631d5
Antti Kivimäki <antti.kivimaki@eatech.fi>
2016-11-24 23:14:29 +0200

    Remove transactionTypes table and specify column nullablility

--- migrations/20161117225616_index.js
+++ migrations/20161117225616_index.js
@@ -5,41 +5,35 @@ exports.up = (knex, Promise) => Promise.all([
             .primary()
             .defaultTo(knex.raw('uuid_generate_v4()'));
         table.string('username', 20)
+            .notNullable()
             .unique();
-        table.string('password');
+        table.string('password')
+            .notNullable();
         table.float('saldo')
+            .notNullable()
             .defaultTo(0);
-    }),
-
-
-    knex.schema.createTable('transactionTypes', table => {
-        table.increments('id')
-            .primary();
-        table.string('typename');
     }),
 
     knex.schema.createTable('transactions', table => {
         table.increments('id')
             .primary();
-        table.integer('typeId')
-            .unsigned()
-            .references('id')
-            .inTable('transactionTypes')
-            .onDelete('SET NULL');
         table.uuid('userId')
+            .notNullable()
             .references('id')
             .inTable('users')
             .onDelete('SET NULL');
         table.timestamp('timestamp')
             .defaultTo(knex.raw('now()'));
-        table.float('oldSaldo');
-        table.float('newSaldo');
-        table.string('comment');
+        table.float('oldSaldo')
+            .notNullable();
+        table.float('newSaldo')
+            .notNullable();
+        table.string('comment')
+            .nullable();
     })
 ]);
 
 exports.down = (knex, Promise) => Promise.all([
     knex.schema.dropTable('transactions'),
-    knex.schema.dropTable('users'),
-    knex.schema.dropTable('transactionTypes')
+    knex.schema.dropTable('users')
 ]);
