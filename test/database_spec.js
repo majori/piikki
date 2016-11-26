1c1f828b3dd01ae81829a9bcef87e7e40c9d0ab2
Antti Kivimäki <antti.kivimaki@eatech.fi>
2016-11-26 15:59:13 +0200

    Fix tests

--- test/database_spec.js
+++ test/database_spec.js
@@ -4,7 +4,9 @@ const Promise   = require('bluebird');
 const _         = require('lodash');
 
 const cfg = require('../config');
-const db = require(`${cfg.buildDir}/database`);
+const knex = require(`${cfg.buildDir}/database`).knex;
+const transactionCore = require(`${cfg.buildDir}/core/transaction-core`);
+const userCore = require(`${cfg.buildDir}/core/user-core`);
 
 const tables = ['transactions', 'users', 'knex_migrations'];
 
@@ -14,30 +16,30 @@ describe('Database', () => {
 
     // Clear tables and migrate to latest
     before(() => Promise.each(tables, table =>
-            db._knex.schema.dropTableIfExists(table)
+            knex.schema.dropTableIfExists(table)
         )
-        .then(() => db._knex.migrate.latest({directory: cfg.migrationDir}))
+        .then(() => knex.migrate.latest({directory: cfg.migrationDir}))
     );
 
     it('should create a new user', () => expect(
-        db.createUser(_.pick(user, ['username', 'password']))
-        .then(() => db.getUser(user.username))).eventually.to.containSubset(_.pick(user, ['username', 'saldo']))
+        userCore.createUser(_.pick(user, ['username', 'password']))
+        .then(() => userCore.getUser(user.username))).eventually.to.containSubset(_.pick(user, ['username', 'saldo']))
     );
 
     it('should authenticate user', () => 
-        expect(db.authenticateUser(user)).to.eventually.be.fulfilled
+        expect(userCore.authenticateUser(user)).to.eventually.be.fulfilled
     );
 
     it('should not authenticate user with wrong password', () => 
-        expect(db.authenticateUser(_.assign(user, { password: 'wrong' }))).to.eventually.be.rejectedWith('Invalid password')
+        expect(userCore.authenticateUser(_.assign(user, { password: 'wrong' }))).to.eventually.be.rejectedWith('Invalid password')
     );
 
     it('should make a transaction', () => {
         let amount = 10;
         
-        return db.makeTransaction(user.username, amount)
-        .then(() => expect(db.getUser(user.username)).to.eventually.containSubset({ saldo: amount }))
-        .then(() => db.makeTransaction(user.username, -amount))
-        .then(() => expect(db.getUser(user.username)).to.eventually.containSubset({ saldo: 0 }));
+        return transactionCore.makeTransaction(user.username, amount)
+        .then(() => expect(userCore.getUser(user.username)).to.eventually.containSubset({ saldo: amount }))
+        .then(() => transactionCore.makeTransaction(user.username, -amount))
+        .then(() => expect(userCore.getUser(user.username)).to.eventually.containSubset({ saldo: 0 }));
     });
 });
