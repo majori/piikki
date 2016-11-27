a5bf1c32c55aa936e478f7e13aa9ecb7da37a3c0
Antti Kivimäki <antti.kivimaki@eatech.fi>
2016-11-27 18:16:06 +0200

    Uglify if production

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp   = require('gulp');
 const ts     = require('gulp-typescript');
 const tslint = require('gulp-tslint');
+const gutil  = require('gulp-util');
+const uglify = require('gulp-uglify');
+const pump   = require('pump');
 const del    = require('del');
 const rs     = require('run-sequence');
 const cfg    = require('./config');
@@ -16,21 +19,27 @@ CONFIG.TS_FILES = `${CONFIG.SOURCE_DIR}/**/*.ts`;
 
 const tsProject = ts.createProject(CONFIG.TS_CONFIG_FILE);
 
-gulp.task('tslint', () => 
-    gulp.src(CONFIG.TS_FILES)
-        .pipe(tslint())
-        .pipe(tslint.report())
+gulp.task('tslint', (cb) =>
+    pump([
+        gulp.src(CONFIG.TS_FILES),
+        tslint(),
+        tslint.report()
+    ])
 );
 
 gulp.task('build-typescript', () => {
     const tsResult = tsProject.src().pipe(tsProject());
-    return tsResult.js.pipe(gulp.dest(CONFIG.BUILD_DIR))
+    return pump([
+        tsResult.js,
+        cfg.isProduction ? uglify() : gutil.noop(),
+        gulp.dest(CONFIG.BUILD_DIR)
+    ]);
 });
 
 gulp.task('clean', () => del('./dist'));
 
 gulp.task('watch', () => {
-    gulp.watch(CONFIG.TS_FILES, ['build-typescript'])
+    gulp.watch(CONFIG.TS_FILES, ['build-typescript']);
 });
 
 gulp.task('lint', ['tslint']);
