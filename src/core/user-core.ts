d967016203c6dcb04c6a7913a46336606362586d
Antti Kivimäki <majoriz11@gmail.com>
2016-12-26 11:35:44 +0200

    Make password hashing async

--- src/core/user-core.ts
+++ src/core/user-core.ts
@@ -44,30 +44,38 @@ export function getUser(username: String) {
 
 // Create new user
 export function createUser(user: IUserDto) {
-    return knex('users').where({username: user.username})
+    return knex.from('users').where({username: user.username})
         .then((records) => _.isEmpty(records) ?
             Promise.resolve() :
             Promise.reject(`Username ${user.username} already exists`)
         )
-        .then(() => knex('users').insert({
+        .then(() => new Promise((resolve, reject) => {
+            bcrypt.hash(user.password, saltRounds, (err, hash) => (err) ?
+                reject(err) :
+                resolve(hash));
+            })
+        )
+        .then((hash) => knex.from('users').insert({
             username: user.username,
-            password: bcrypt.hashSync(user.password, saltRounds)
+            password: hash
         }));
 };
 
 // Puts user's "active" -status to false
 export function deleteUser(username: String) {
-    return knex('users').where({ username }).update({ active: false })
+    return knex.from('users').where({ username }).update({ active: false })
         .then(() => Promise.resolve());
 }
 
 // Compare raw password with the hashed one
 export function authenticateUser(user: IUserDto) {
     return userExists(user.username)
-    .then((row) => bcrypt.compareSync(user.password, row.password) ?
-        Promise.resolve() :
-        Promise.reject('Invalid password'),
-    );
+    .then((row) => new Promise((resolve, reject) => {
+        bcrypt.compare(user.password, row.password, (err, same) => (same) ?
+            resolve() :
+            reject('Invalid password'),
+        );
+    }));
 };
 
 export function createSaldoForUser(username: String, groupName: String) {
@@ -75,14 +83,15 @@ export function createSaldoForUser(username: String, groupName: String) {
         userExists(username),
         groupExists(groupName)
     ])
-    .spread((user: IDatabaseUser, group: IDatabaseGroup) => knex('user_saldos')
+    .spread((user: IDatabaseUser, group: IDatabaseGroup) => knex
+        .from('user_saldos')
         .insert({group_id: group.id, user_id: user.id})
     );
 };
 
 // Checks if user is in database
 export function userExists(username: String) {
-    return knex('users').where({ username }).first()
+    return knex.from('users').where({ username }).first()
         .then((row) => _.isUndefined(row) ?
             Promise.reject(`User ${username} not found`) :
             Promise.resolve(row)
