bb8ee3f8b56e94d5db8348dce4697ec0dd650985
Antti Kivimäki <antti.kivimaki@eatech.fi>
2017-04-16 19:12:17 +0300

    Discard rollback commands because we don't want get back to decimal(18,0) datatype

--- migrations/20170416013136_saldo_precision.js
+++ migrations/20170416013136_saldo_precision.js
@@ -6,10 +6,4 @@ exports.up = (knex, Promise) => {
   ]);
 };
 
-exports.down = (knex, Promise) => {
-  return Promise.all([
-    knex.schema.raw('ALTER TABLE transactions ALTER COLUMN new_saldo decimal(18,0) not null;'),
-    knex.schema.raw('ALTER TABLE transactions ALTER COLUMN old_saldo decimal(18,0) not null;'),
-    knex.schema.raw('ALTER TABLE user_saldos ALTER COLUMN saldo decimal(18,0) not null;'),
-  ]);
-};
+exports.down = (knex, Promise) => Promise.resolve();
