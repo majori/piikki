9234d6aca99f6011ff42233cf3896a9e4ecf5fa5
Antti Kivimäki <majoriz11@gmail.com>
2017-04-30 11:26:01 +0300

    Add new user and group to seed data

--- seeds/development.js
+++ seeds/development.js
@@ -14,7 +14,8 @@ exports.seed = (knex, Promise) => {
       "SET IDENTITY_INSERT users ON;" +
       "INSERT INTO users(id, username, password) VALUES " +
       `(1,'testUser1','${bcrypt.hashSync('1234', 6)}'),` +
-      `(2,'testUser2','${bcrypt.hashSync('4321', 6)}');` +
+      `(2,'testUser2','${bcrypt.hashSync('4321', 6)}'),` +
+      `(3,'testUser3','${bcrypt.hashSync('1243', 6)}');` +
       "SET IDENTITY_INSERT users OFF;"
     ))
 
@@ -22,7 +23,8 @@ exports.seed = (knex, Promise) => {
     .then(() => knex.raw(
       "SET IDENTITY_INSERT groups ON;" +
       "INSERT INTO groups(id, name) VALUES " +
-      "(1,'group1');" +
+      "(1,'group1')," +
+      "(2,'group2');" +
       "SET IDENTITY_INSERT groups OFF;"
     ))
 
@@ -32,7 +34,9 @@ exports.seed = (knex, Promise) => {
       "INSERT INTO transactions"+
       "(id, user_id, group_id, old_saldo, new_saldo) VALUES " +
       "(1,1,1,0,10)," +
-      "(2,2,1,0,-10);" +
+      "(2,2,1,0,-10)," +
+      "(3,3,1,0,5)," +
+      "(4,3,2,0,-5);" +
       "SET IDENTITY_INSERT transactions OFF;"
     ))
 
@@ -41,7 +45,9 @@ exports.seed = (knex, Promise) => {
       "SET IDENTITY_INSERT user_saldos ON;" +
       "INSERT INTO user_saldos(id, user_id, group_id, saldo) VALUES " +
       "(1,1,1,10)," +
-      "(2,2,1,-10);" +
+      "(2,2,1,-10)," +
+      "(3,3,1,5)," +
+      "(4,3,2,-5);" +
       "SET IDENTITY_INSERT user_saldos OFF;"
     ))
 
