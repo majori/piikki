e1ee4505e2b712b5a18a581e84739682e1f0b162
Antti Kivimäki <antti.kivimaki@eatech.fi>
2017-06-06 21:48:32 +0300

    Create seed data dynamically

--- seeds/data/dev.js
+++ seeds/data/dev.js
@@ -1,99 +1,68 @@
+const faker = require('faker');
+const _ = require('lodash');
+const moment = require('moment');
+
+const USER_AMOUNT = 80;
+
+const users = _.times(USER_AMOUNT, () => ({
+  username: faker.internet.userName(),
+  password: '1234'
+}));
+
+const groups = _.times(5, (i) => ({ groupName: `group${i}` }));
+
+const userSaldos = _.concat(
+  _.times(_.round(USER_AMOUNT * 0.6), () => ({
+    username: _.sample(users).username,
+    groupName: `group${_.random(3)}`,
+    saldo: _.random(-10, 10)
+  })),
+  _.times(_.round(USER_AMOUNT * 0.1), () => ({
+    username: _.sample(users).username,
+    groupName: `group${_.random(3)}`,
+    saldo: _.random(-2, 2)
+  }))
+);
+
+const tokens = [
+  {
+    token: 'restricted_token',
+    role: 'restricted',
+    comment: 'For restricted'
+  },
+  {
+    token: 'global_token',
+    role: 'global',
+    comment: 'For global'
+  },
+  {
+    token: 'admin_token',
+    role: 'admin',
+    comment: 'For admin'
+  }
+];
+
+const tokenGroupAccess = [
+  {
+    groupName: 'group1',
+    token: 'restricted_token'
+  }
+];
+
+const transactions = _.map(userSaldos, saldo => ({
+  username: saldo.username,
+  groupName: saldo.groupName,
+  token: tokens[_.random(1)].token,
+  timestamp: moment().subtract(_.random(15), 'days').format('YYYY-MM-DD HH:mm:ss.SSS'),
+  oldSaldo: 0,
+  newSaldo: saldo.saldo
+}));
+
 module.exports = {
-  "users": [
-    {
-      "username": "testUser1",
-      "password": "1234"
-    },
-    {
-      "username": "testUser2",
-      "password": "4321"
-    },
-    {
-      "username": "testUser3",
-      "password": "1243"
-    },
-    {
-      "username": "testUser4",
-      "password": "4312"
-    }
-  ],
-  "groups": [
-    { "groupName": "group1" },
-    { "groupName": "group2" }
-  ],
-  "userSaldos": [
-    {
-      "username": "testUser1",
-      "groupName": "group1",
-      "saldo": 10
-    },
-    {
-      "username": "testUser2",
-      "groupName": "group1",
-      "saldo": -10
-    },
-    {
-      "username": "testUser3",
-      "groupName": "group2",
-      "saldo": 5
-    },
-    {
-      "username": "testUser3",
-      "groupName": "group2",
-      "saldo": -5
-    }
-  ],
-  "tokens": [
-    {
-      "token": "restricted_token",
-      "role": "restricted",
-      "comment": "For restricted"
-    },
-    {
-      "token": "global_token",
-      "role": "global",
-      "comment": "For global"
-    },
-    {
-      "token": "admin_token",
-      "role": "admin",
-      "comment": "For admin"
-    }
-  ],
-  "tokenGroupAccess": [
-    {
-      "groupName": "group1",
-      "token": "restricted_token"
-    }
-  ],
-  "transactions": [
-    {
-      "username": "testUser1",
-      "groupName": "group1",
-      "token": "restricted_token",
-      "oldSaldo": 0,
-      "newSaldo": 10
-    },
-    {
-      "username": "testUser2",
-      "groupName": "group1",
-      "token": "restricted_token",
-      "oldSaldo": 0,
-      "newSaldo": -10
-    },
-    {
-      "username": "testUser3",
-      "groupName": "group2",
-      "token": "global_token",
-      "oldSaldo": 0,
-      "newSaldo": 5
-    },
-    {
-      "username": "testUser3",
-      "groupName": "group2",
-      "token": "global_token",
-      "oldSaldo": 0,
-      "newSaldo": -5
-    }
-  ]
+  users,
+  groups,
+  userSaldos,
+  transactions,
+  tokens,
+  tokenGroupAccess,
 }
