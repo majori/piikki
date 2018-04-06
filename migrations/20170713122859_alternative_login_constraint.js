924acc2fb5e2caf85b830716f72d926b108b2429
Antti Kivimäki <antti.kivimaki@futurice.com>
2018-04-06 11:13:19 +0300

    Remove constraint workaround

--- migrations/20170713122859_alternative_login_constraint.js
+++ migrations/20170713122859_alternative_login_constraint.js
@@ -1,13 +1,8 @@
-const constraintName = 'alternative_login_group_id_hashed_key_unique';
 
-// For some reason mssql set constraint type to "Index"
-// if table.unique(['group_id', 'hashed_key']) is used
-exports.up = (knex, Promise) => knex.schema.raw(
-  `ALTER TABLE alternative_login
-  ADD CONSTRAINT ${constraintName}
-  UNIQUE (group_id, hashed_key)`
-);
+exports.up = (knex, Promise) => knex.schema.alterTable('alternative_login', (t) => {
+  t.unique(['group_id', 'hashed_key'])
+});
 
 exports.down = (knex, Promise) => knex.schema.alterTable('alternative_login', (t) => {
-  t.dropUnique(['group_id', 'hashed_key'], constraintName);
+  t.dropUnique(['group_id', 'hashed_key']);
 });
